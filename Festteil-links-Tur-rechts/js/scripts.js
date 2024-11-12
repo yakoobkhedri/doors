95734857625e9e24427368ac2bee22eee030fe4e
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-11-12 14:52:59 +0330

    commit message

--- Festteil-links-Tur-rechts/js/scripts.js
+++ Festteil-links-Tur-rechts/js/scripts.js
@@ -2,29 +2,27 @@ function changeSize() {
 
     // let size_www = parseInt(document.getElementById('www').value);
 
+    let size_right = parseInt(document.getElementById('right').value);
     let size_front = parseInt(document.getElementById('front').value);
-    let size_left = parseInt(document.getElementById('left').value);
     var size_height = parseInt(document.getElementById('height').value);
 
 
+    size_right = size_right / 19;
     size_front = size_front / 19;
-    size_left = size_left / 19;
     size_height = size_height / 18;
 
+    GlasRight.style.width = size_right + 'mm';
+    GlasRight.style.height = size_height + 'mm';
+
+    size_terst = size_right / 5;
     size_terstR = size_front / 5;
-    size_terstll = size_left / 5;
+
+    GlasRight.style.top = 56 + size_terstR + 'mm';
+    GlasRight.style.left = 78 + size_front + 'mm';
 
     GlasFront.style.height = size_height + 'mm';
     GlasFront.style.width = size_front + 'mm';
-    GlasFront.style.top = 56 + 'mm';
-
-    // شیشه سمت چپ را باید تست بکنی وبراش ایف بزنی
-
-    GlasLeft.style.height = size_height + 'mm';
-    GlasLeft.style.width = size_left + 'mm';
-    GlasLeft.style.top = 58 - size_terstll - size_terstR + 'mm';
-
-
+    GlasFront.style.top = 56 + size_terst + 'mm';
 
 
     // if(size_front > 15 && size_front <=26){ //Glasswidth 960-1000
@@ -86,10 +84,12 @@ $(document).ready(function () {
 });
 
 
-// Tür Recht
+
+// Tür Links
 $(document).ready(function () {
-    $('#left').on('change', function () {
-        var width = $('#left').val();
+
+    $('#right').on('change', function () {
+        var width = $('#right').val();
         if (width >= 150 && width <= 2200) {
 
             changeSize()
